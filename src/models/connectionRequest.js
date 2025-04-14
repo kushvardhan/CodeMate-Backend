e55827bb4f64477ebe2d035cb2ca5d080b747528
Kush Vardhan <kushvardhan39797@gmail.com>
2025-04-14 19:05:22 +0530

    Refactor connection request model and enhance request router validation

--- src/models/connectionRequest.js
+++ src/models/connectionRequest.js
@@ -1,25 +1,30 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const connectionRequestSchema = new mongoose.Schema({
-    fromUserId:{
-        type : mongoose.Schema.Types.ObjectId,
-        required : true,
+const connectionRequestSchema = new mongoose.Schema(
+  {
+    fromUserId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
     },
-    toUserId:{
-        type : mongoose.Schema.Types.ObjectId,
-        required : true,
+    toUserId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
     },
-    status:{
-        type:String,
-        required : true,
-        enum : {
-            values : ["ignore","interested","accepted","rejected"],
-            message : '{VALUE} is not supported'
-        },
-        
-    }
-},{timestamps:true});
+    status: {
+      type: String,
+      required: true,
+      enum: {
+        values: ["ignored", "interested", "accepted", "rejected"],
+        message: "{VALUE} is not supported",
+      },
+    },
+  },
+  { timestamps: true }
+);
 
-const ConnectionRequest = new mongoose.model("ConnectionRequest",connectionRequestSchema);
+const ConnectionRequest = new mongoose.model(
+  "ConnectionRequest",
+  connectionRequestSchema
+);
 
 module.exports = ConnectionRequest;
--- src/routes/requestRouter.js
+++ src/routes/requestRouter.js
@@ -1,35 +1,60 @@
 const express = require("express");
 const router = express.Router();
-const {userAuth} = require("../middleware/auth"); 
+const { userAuth } = require("../middleware/auth");
 const ConnectionRequest = require("../models/connectionRequest");
+const User = require("../models/user");
+const mongoose = require("mongoose");
 
-router.post('/send/:status/:toUserId',userAuth,async(req,res)=>{
-    try{    
-        const fromUserId = req.user._id;
-        const toUserId = req.params.toUserId;
-        const status = req.params.status;
+router.post("/send/:status/:toUserId", userAuth, async (req, res) => {
+  try {
+    const fromUserId = req.user._id;
+    const toUserId = req.params.toUserId;
+    const status = req.params.status;
 
-        if(status !== "ignored" && status !== "interested"){
-            res.status(500).json({ message: "Invalid status." });
-        }
-
-        const connectionRequest = new ConnectionRequest({
-            fromUserId,
-            toUserId,
-            status
-        });
-
-        if(!connectionRequest){
-            return res.status(400).json({ message: "Request not sent." });
-        }
-
-        const data = await connectionRequest.save();
-        res.status(200).json({ message: "Request sent successfully.", data:data });
-
-    }catch(err){
-        res.status(500).json({ message: "Internal server error."+err.message });
+    if (!mongoose.Types.ObjectId.isValid(toUserId)) {
+      return res.status(400).json({ message: "Invalid user ID format." });
     }
-})
 
+    const allowedStatus = ["ignored", "interested"];
+
+    if (!allowedStatus.includes(status)) {
+      return res.status(400).json({
+        message: "Invalid status. Only 'ignored' and 'interested' are allowed.",
+      });
+    }
+
+
+    const existingConnectionRequest = await ConnectionRequest.findOne({
+      $or: [
+        {
+          fromUserId,
+          toUserId,
+        },
+        {
+          fromUserId: toUserId,
+          toUserId: fromUserId,
+        },
+      ],
+    });
+    if (existingConnectionRequest) {
+      return res.status(400).json({ message: "Request already sent." });
+    }
+
+    const connectionRequest = new ConnectionRequest({
+      fromUserId,
+      toUserId,
+      status,
+    });
+
+    if (!connectionRequest) {
+      return res.status(400).json({ message: "Request not sent." });
+    }
+
+    const data = await connectionRequest.save();
+    res.status(200).json({ message: "Request sent successfully.", data: data });
+  } catch (err) {
+    res.status(500).json({ message: "Internal server error." + err.message });
+  }
+});
 
 module.exports = router;
