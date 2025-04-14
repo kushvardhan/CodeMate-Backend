3d389af8e5e89be8b006a3726aadcd905876024a
Kush Vardhan <kushvardhan39797@gmail.com>
2025-04-14 23:28:10 +0530

    Enhance connections retrieval logic and implement feed endpoint to exclude hidden users

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -52,13 +52,27 @@ router.get("/request/connections", userAuth, async (req, res) => {
         "about",
         "gender",
         "age",
+      ]).populate("toUserId", [
+        "firstName",
+        "lastName",
+        "photoUrl",
+        "skills",
+        "about",
+        "gender",
+        "age",
       ]);
 
     if (connectionRequests.length === 0) {
       return res.status(404).json({ message: "No connections found." });
     }
 
-    const connections = connectionRequests.map((request) => request.fromUserId);
+    const connections = connectionRequests.map((request) => {
+        if(request.fromUserId._id.toString() === loggedInUser._id.toString()){
+            return request.toUserId;
+        }else{
+            return request.fromUserId;
+        }
+    });
 
     res.status(200).json({
       message: "Connections fetched successfully.",
@@ -68,5 +82,36 @@ router.get("/request/connections", userAuth, async (req, res) => {
     res.status(500).json({ message: err.message });
   }
 });
+
+router.get("/feed",userAuth,async(req,res)=>{
+    try{
+        const loggedInUser = req.user;
+
+        const connectionRequest = await ConnectionRequest.find({
+            $or: [
+                { fromUserId: loggedInUser._id},
+                { toUserId: loggedInUser._id},
+            ],
+        }).select('fromUserId toUserId');
+
+        const hideUserFromFeed = new Set();
+        connectionRequest.forEach((req)=>{
+            hideUserFromFeed.add(req.fromUserId.toString());
+            hideUserFromFeed.add(req.toUserId.toString());
+        })
+        console.log(hideUserFromFeed);
+
+        const user = await User.find({
+           $and : [{_id: { $nin: Array.from(hideUserFromFeed) },},
+                   { _id: { $ne: loggedInUser._id } }
+        ],
+        }).select('firstName lastName photoUrl skills about gender age');
+
+        res.json(user);
+
+    }catch(err){
+        res.status(500).json({ message: err.message });
+    }
+})
 
 module.exports = router;
