ea8082a5949413b89813401c857bb3bc5532d989
Kush Vardhan <kushvardhan39797@gmail.com>
2025-04-27 20:52:00 +0530

    Enhance CORS configuration to include PATCH method and improve validation error handling for signup and login processes

--- src/app.js
+++ src/app.js
@@ -13,8 +13,8 @@ const app = express();
 app.use(
   cors({
     origin: ["http://localhost:5173", "http://localhost:5174"],
-    credentials: true, 
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
--- src/utils/validation.js
+++ src/utils/validation.js
@@ -1,52 +1,73 @@
-const validator = require('validator');
+const validator = require("validator");
 
 const validateSignupData = (req) => {
-    try {
-        if (!req || !req.body) throw new Error('Invalid input.');
-        const { firstName, lastName, email, password } = req.body;
+  try {
+    if (!req || !req.body) throw new Error("Invalid input.");
+    const { firstName, lastName, email, password } = req.body;
 
-        if (!firstName) throw new Error('Enter Name');
-        else if (!firstName.trim() || !email.trim() || !password.trim()) throw new Error('Enter credentials.');
-        if (firstName.length < 3 || firstName.length > 20) throw new Error('Name must be 3-20 characters.');
-        if (!validator.isEmail(email)) throw new Error('Enter a valid email.');
-        if (!validator.isStrongPassword(password)) 
-            throw new Error('Password must be at least 8 characters long, containing at least 1 uppercase, 1 lowercase, 1 number, and 1 special character.');
-        
-    } catch (err) {
-        throw new Error(err.message);
-    }
+    if (!firstName) throw new Error("Enter Name");
+    else if (!firstName.trim() || !email.trim() || !password.trim())
+      throw new Error("Enter credentials.");
+    if (firstName.length < 3 || firstName.length > 20)
+      throw new Error("Name must be 3-20 characters.");
+    if (!validator.isEmail(email)) throw new Error("Enter a valid email.");
+    if (!validator.isStrongPassword(password))
+      throw new Error(
+        "Password must be at least 8 characters long, containing at least 1 uppercase, 1 lowercase, 1 number, and 1 special character."
+      );
+  } catch (err) {
+    throw new Error(err.message);
+  }
 };
 
 const validateLoginData = (req) => {
-    try {
+  try {
+    const { email, password } = req.body;
 
-        const { email, password } = req.body;
-
-        if (!email.trim() || !password.trim()) throw new Error('Enter credentials.');
-        if (!validator.isEmail(email)) throw new Error('Invalid email credentials.');
-        if (!validator.isStrongPassword(password)) 
-            throw new Error('Password must be at least 8 characters long, containing at least 1 uppercase, 1 lowercase, 1 number, and 1 special character.');
-    } catch (err) {
-        throw new Error('Invalid credentials.:'+err.message); 
-    }
+    if (!email.trim() || !password.trim())
+      throw new Error("Enter credentials.");
+    if (!validator.isEmail(email))
+      throw new Error("Invalid email credentials.");
+    if (!validator.isStrongPassword(password))
+      throw new Error(
+        "Password must be at least 8 characters long, containing at least 1 uppercase, 1 lowercase, 1 number, and 1 special character."
+      );
+  } catch (err) {
+    throw new Error("Invalid credentials.:" + err.message);
+  }
 };
 
 const validateProfileData = (req) => {
-    try {
-        if (!req || !req.body) throw new Error('Invalid input.');
-        const allowedEditFields = ["firstName", "lastName", "photoUrl", "gender", "age", "skills", "about"];
-        const invalidFields = Object.keys(req.body).every(field => allowedEditFields.includes(field));
-        
-        return invalidFields;
-        
-    } catch (err) {
-        throw new Error(err.message);
-    }
-};
- 
+  try {
+    if (!req || !req.body) throw new Error("Invalid input.");
+    const allowedEditFields = [
+      "firstName",
+      "lastName",
+      "photoUrl",
+      "gender",
+      "age",
+      "skills",
+      "about",
+      "location",
+    ];
 
-module.exports = { 
-    validateSignupData,
-    validateLoginData,
-    validateProfileData
-}
+    // Check if every field in the request body is in the allowed fields list
+    const hasInvalidFields = Object.keys(req.body).some(
+      (field) => !allowedEditFields.includes(field)
+    );
+
+    if (hasInvalidFields) {
+      throw new Error("Email and password cant be changed.");
+    }
+
+    return true;
+  } catch (err) {
+    throw new Error(err.message);
+  }
+};
+
+module.exports = {
+  validateSignupData,
+  validateLoginData,
+  validateProfileData,
+};
