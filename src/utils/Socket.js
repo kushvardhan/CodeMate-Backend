9eb6bd545637c459e5540dc98b0fa4f06dc7c902
Kush Vardhan <kushvardhan39797@gmail.com>
2025-05-10 20:36:49 +0530

    Configured Events

--- src/utils/Socket.js
+++ src/utils/Socket.js
@@ -1,29 +1,42 @@
-const socket = require('socket.io');
+const socket = require("socket.io");
 
 const initialzeSocket = (server) => {
-    
-    const  io = socket(server,{
-        cors:{
-            origin: "http://localhost:5173",
-            credentials: true,
-        }
+  const io = socket(server, {
+    cors: {
+      origin: "http://localhost:5173",
+      credentials: true,
+    },
+  });
+
+  io.on("connection", (socket) => {
+    console.log("A user connected");
+
+    socket.on("joinChat", (data) => {
+      const { loggedInUserId, userId } = data;
+      console.log("User joined chat:", data);
+      const roomName = [loggedInUserId, userId].sort().join("-");
+      socket.join(roomName);
+      console.log(`User ${loggedInUserId} joined room: ${roomName}`);
     });
 
-    io.on('connection', (socket) => {
-        console.log('A user connected');
+    socket.on("sendMessage", (data) => {
+      const { senderId, receiverId, content } = data;
+      console.log("Message received:", data);
 
-        socket.io('joinChat',()=>{
+      const roomName = [senderId, receiverId].sort().join("-");
 
-        })
-        socket.io('sendMessage',()=>{
-
-        })
-        socket.io('disconnect',()=>{
-            
-        })
-
+      // Emit the message to everyone in the room except the sender
+      socket.to(roomName).emit("receiveMessage", {
+        senderId,
+        content,
+        timestamp: new Date().toISOString(),
+      });
     });
 
+    socket.on("disconnect", () => {
+      console.log("User disconnected");
+    });
+  });
 };
 
 module.exports = initialzeSocket;
