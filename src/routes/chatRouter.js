7201268e8530e559bf0a7de0f1362719a34cc02c
Kush Vardhan <kushvardhan39797@gmail.com>
2025-05-11 13:36:10 +0530

    made change in  the (!chat){..}

--- src/routes/chatRouter.js
+++ src/routes/chatRouter.js
@@ -4,39 +4,42 @@ const { userAuth } = require("../middleware/auth");
 const User = require("../models/user");
 const Chat = require('../models/chat');
 
-router.get('/getChat/:userId',userAuth,async(req,res)=>{
-    try{
-        const {userId} = req.params;
+router.get('/getChat/:userId', userAuth, async (req, res) => {
+    try {
+        const { userId } = req.params;
         const loggedUser = req.user._id;
 
-        if( !loggedUser || !userId){
+        if (!loggedUser || !userId) {
             return res.status(400).json({ message: "Missing required user IDs" });
         }
 
-        const chat = await Chat.findOne({
+        let chat = await Chat.findOne({
             participants: { $all: [loggedUser, userId] },
-          }).populate({
+        }).populate({
             path: 'messages.senderId',
             select: 'firstName lastName photoUrl',
-          }) ;
+        });
 
-          if (!chat) {
+        if (!chat) {
             chat = new Chat({
-              participants: [loggedUser, userId],
-              messages: [],
-            }).populate({
-            path: 'messages.senderId',
-            select: 'firstName lastName photoUrl',
-          }) ;
+                participants: [loggedUser, userId],
+                messages: [],
+            });
 
             await chat.save();
-          }
 
-          res.status(200).json(chat.messages);
+            // Re-fetch the chat to populate messages.senderId
+            chat = await Chat.findById(chat._id).populate({
+                path: 'messages.senderId',
+                select: 'firstName lastName photoUrl',
+            });
+        }
 
-    }catch(err){
+        res.status(200).json(chat.messages);
+    } catch (err) {
         res.status(500).json({ message: err.message });
     }
 });
+
 
 module.exports = router;
