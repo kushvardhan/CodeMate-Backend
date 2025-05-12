9e309c3079862a9393349b56c644b6b2669abdea
Kush Vardhan <kushvardhan39797@gmail.com>
2025-05-12 19:54:03 +0530

    excluded the email,password and others thing while fetching the data from DB in the /chatUser/:id

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -147,7 +147,8 @@ router.get("/feed", userAuth, async (req, res) => {
 router.post("/chatUser/:id", userAuth, async (req, res) => {
   try{
     const {id} = req.params;
-    const user = await User.findById(id);
+    
+    const user = await User.findById(id).select('-email -password -skills -location');
     res.status(200).json({ message: "User fetched successfully.", data: user });
   }catch(err){
     console.log('error ',err);
